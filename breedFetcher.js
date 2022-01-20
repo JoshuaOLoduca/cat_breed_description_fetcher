94d190eb572741c95a2ffe3867f4214b22697e00
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-01-20 18:58:06 +0000

    refactor

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,29 @@
 const request = require('request');
-const breedName = process.argv[2];
+// const breedName = process.argv[2];
 
-request('https://api.thacatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
-  if (error !== null) {
-    console.log(error);
-    console.log('Website Not Found');
-    return;
-  }
-  const data = JSON.parse(body);
 
-  if (data.length === 0) {
-    console.log(breedName,'Not found');
-    return;
-  }
+const fetchBreedDescription = function(breedName, callback) {
 
-  const desc = data[0].description;
-  console.log(desc);
-});
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+    if (error !== null) {
+      callback(error);
+      return;
+    }
+    
+    const data = JSON.parse(body);
+    
+    if (data.length === 0) {
+      callback(breedName + ' Not found');
+      return;
+    }
+    
+    const desc = data[0].description;
+    callback(null, desc);
+  });
+  
+};
+
+// const breedDescription = fetchBreedDescription(breedName, (error, description) => {
+
+// });
+module.exports = { fetchBreedDescription };
